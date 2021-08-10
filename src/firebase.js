0b3b861bc8169315a1086ee60744937284b40df5
FalconRam <ramvignesh974@gmail.com>
2021-08-10 17:01:37 +0530

    end

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3s4FV6fkTC6cMEV8nQaZOvFcVl2zOhVo",
+  authDomain: "tesla-clone-67811.firebaseapp.com",
+  projectId: "tesla-clone-67811",
+  storageBucket: "tesla-clone-67811.appspot.com",
+  messagingSenderId: "743420205321",
+  appId: "1:743420205321:web:79521fc16a3cc3193adbd8",
+  measurementId: "G-CQ5VQL9MW2",
+};
